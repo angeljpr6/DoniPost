9d8189ef8935652994f070113e5428165d76b7b8
angeljpr6 <angelp.r806@gmail.com>
2024-05-15 23:00:15 +0100

    Header de prueba hecho

--- Aprendiendo/src/App.js
+++ Aprendiendo/src/App.js
@@ -1,35 +1,21 @@
-import logo from './logo.svg';
+import logo from './assets/images/logo.svg';
 import './assets/css/App.css';
 import Componente from './components/Componente';
 import SeccionPruebas from './components/SeccionPruebas';
 import Posts from './components/Posts';
+import Header from './components/Header';
 
 
 function App() {
   var usuario="Fede"
   return (
     <div className="App">
+      
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Posts
-        />
-
-       
-
-
-
+        <Header/>
+        
       </header>
     </div>
   );
--- Aprendiendo/src/components/Posts.js
+++ Aprendiendo/src/components/Posts.js
@@ -23,10 +23,6 @@ class Posts extends Component {
 
 
     render() {
-        
-        
-
-
         return (
             <div>
                 <h1>POSTS</h1>
