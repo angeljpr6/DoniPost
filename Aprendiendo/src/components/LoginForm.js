498e6c8d57811dbbe05cba9fef167488564b0bba
danielperezalejandro <danieloctaviosegundo@gmail.com>
2024-05-18 17:18:48 +0100

    inicio muestra seguidos

--- Aprendiendo/src/components/LoginForm.js
+++ Aprendiendo/src/components/LoginForm.js
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { setUser } from '../Recursos/UserLogin';
+
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const navigate = useNavigate();
+  
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:3900/api/login', { // Asegúrate de que esta URL es correcta
+      const response = await fetch('http://localhost:3900/api/login', { 
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -26,13 +29,14 @@ const LoginForm = () => {
       const result = await response.json();
 
       if (response.status === 200) {
+        setUser(result.user.name)
         navigate('/Inicio');
       } else {
         setErrorMessage(result.message);
       }
     } catch (error) {
       console.error('Error during login:', error);
-      setErrorMessage('Login failed, please try again.');
+      setErrorMessage('Usuario o contraseña incorrectos');
     }
   };
 
