f9dcefc3a83b9fc7059d3ec49ddc0a485c73a9fa
angeljpr6 <angelp.r806@gmail.com>
2024-05-18 18:09:19 +0100

    Arrelgo de fallos

--- Aprendiendo/src/components/Perfil.js
+++ Aprendiendo/src/components/Perfil.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 import { getUser } from "../Recursos/UserLogin";
 import PostUser from "./PostUser";
+import Header from "./Header";
+import BarraLateral from "./BarraLateral";
 
 class Perfil extends Component{
 
@@ -9,20 +11,25 @@ class Perfil extends Component{
         const nombreUsuario = getUser()
         console.log(nombreUsuario)
         return(
-            <div id="contenido">
-                
-                <div id="perfil">
-                    <div id="infoPerfil">
-                        <h1>{nombreUsuario}</h1>
-                        <div id="bioUsuario">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tristique molestie rutrum. Aenean vel auctor nibh, id eleifend urna. In varius nulla eget velit hendrerit, non euismod neque interdum. Pellentesque quam tortor, posuere quis dui ut, euismod vulputate lacus. Nam ultricies eget ipsum ac ultricies. Praesent a mauris dictum nisl imperdiet fermentum id aliquam est. Donec egestas vel urna in viverra. Sed vitae porta dui. Curabitur eget aliquam metus, id venenatis odio. Nam quis mauris eu nulla faucibus lacinia efficitur at mi. Curabitur at ligula orci. Nullam id tristique nibh.</p>
+
+            <div>
+                <Header/>
+                <BarraLateral/>
+                <div id="contenido">
+                    
+                    <div id="perfil">
+                        <div id="infoPerfil">
+                            <h1>{nombreUsuario}</h1>
+                            <div id="bioUsuario">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tristique molestie rutrum. Aenean vel auctor nibh, id eleifend urna. In varius nulla eget velit hendrerit, non euismod neque interdum. Pellentesque quam tortor, posuere quis dui ut, euismod vulputate lacus. Nam ultricies eget ipsum ac ultricies. Praesent a mauris dictum nisl imperdiet fermentum id aliquam est. Donec egestas vel urna in viverra. Sed vitae porta dui. Curabitur eget aliquam metus, id venenatis odio. Nam quis mauris eu nulla faucibus lacinia efficitur at mi. Curabitur at ligula orci. Nullam id tristique nibh.</p>
+                            </div>
+                            <div id="datosPerfil">
+                                <p><a href="#"><strong>Seguidos</strong> 40 </a><a href="#"><strong>Seguidores</strong> 40</a></p>
+                            </div>
                         </div>
-                        <div id="datosPerfil">
-                            <p><a href="#"><strong>Seguidos</strong> 40 </a><a href="#"><strong>Seguidores</strong> 40</a></p>
+                        <div id="postsContent">
+                            <PostUser />
                         </div>
-                    </div>
-                    <div id="postsContent">
-                        <PostUser />
                     </div>
                 </div>
             </div>
