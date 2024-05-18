7021c7accb513973676809381ff71f3815dc98c8
danielperezalejandro <danieloctaviosegundo@gmail.com>
2024-05-18 19:19:37 +0100

    login bonito

--- Aprendiendo/src/components/LoginForm.js
+++ Aprendiendo/src/components/LoginForm.js
@@ -5,66 +5,71 @@ import { setBiography } from '../Recursos/UserLogin';
 
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const navigate = useNavigate();
-  
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
 
-    try {
-      const response = await fetch('http://localhost:3900/api/login', { 
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ name: username, password })
-      });
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
+        try {
+            const response = await fetch('http://localhost:3900/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ name: username, password })
+            });
 
-      const result = await response.json();
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
 
-      if (response.status === 200) {
-        setUser(result.user.name)
-        setBiography(result.user.biography)
-        navigate('/Inicio');
-      } else {
-        setErrorMessage(result.message);
-      }
-    } catch (error) {
-      console.error('Error during login:', error);
-      setErrorMessage('Usuario o contraseña incorrectos');
-    }
-  };
+            const result = await response.json();
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h2>Iniciar sesión</h2>
-        <input
-          type="text"
-          placeholder="Nombre de usuario"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Ingresar</button>
-        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-      </form>
-    </div>
-  );
+            if (response.status === 200) {
+                setUser(result.user.name)
+                setBiography(result.user.biography)
+                navigate('/Inicio');
+            } else {
+                setErrorMessage(result.message);
+            }
+        } catch (error) {
+            console.error('Error during login:', error);
+            setErrorMessage('Usuario o contraseña incorrectos');
+        }
+    };
+
+    return (
+        <div id="login-form">
+            <form id="login-form-inner" onSubmit={handleSubmit}>
+                <div>
+                    <span id="brand">
+                        <strong>Doni</strong>Post
+                    </span>
+                </div>
+                <h2 id="login-title">Iniciar sesión</h2>
+                <input id="login-username"
+                    type="text"
+                    placeholder="Nombre de usuario"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+                <input id="login-password"
+                    type="password"
+                    placeholder="Contraseña"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <button id="login-button" type="submit">Ingresar</button>
+                {errorMessage && <p id="login-error-message">{errorMessage}</p>}
+            </form>
+        </div>
+    );
 };
 
 export default LoginForm;
