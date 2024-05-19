9203e5a7815fc6085e13d5bda28e7e08d00fbde6
angeljpr6 <angelp.r806@gmail.com>
2024-05-19 15:17:02 +0100

    modal creado

--- Aprendiendo/src/components/BarraLateral.js
+++ Aprendiendo/src/components/BarraLateral.js
@@ -1,24 +1,45 @@
 const { Component } = require("react");
 
-
 class BarraLateral extends Component {
-    
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showModal: false
+        };
+    }
 
+    handleEscribirPostClick = () => {
+        this.setState({ showModal: true });
+    }
+
+    handleCloseModal = () => {
+        this.setState({ showModal: false });
+    }
+
+    render() {
         return (
             <div id="barraLat">
                 <a href="/Inicio">
                     <h2>Inicio</h2>
                 </a>
-                <a href="/Perfil" >
+                <a href="/Perfil">
                     <h2>Perfil</h2>
                 </a>
                 <a href="/Perfil">
                     <h2>Opciones</h2>
                 </a>
-                <a href="/Perfil">
+                <a href="#" onClick={this.handleEscribirPostClick}>
                     <h2>Escribir Post</h2>
                 </a>
+                {this.state.showModal && (
+                    <div className="modal">
+                        <div className="modal-content">
+                            <span className="close" onClick={this.handleCloseModal}>&times;</span>
+                            <h1>Modal</h1>
+                            {/* Aquí puedes añadir más contenido al modal si lo necesitas */}
+                        </div>
+                    </div>
+                )}
             </div>
         )
     }
